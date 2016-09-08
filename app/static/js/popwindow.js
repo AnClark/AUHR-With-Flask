af8d457974a932342c4120fe0c34563e027af72b
AnClark <clarklaw4701@qq.com>
2016-09-08 15:40:33 +0000

    Use url_for() function to query CSS and JS.

---
+++ app/static/js/popwindow.js
@@ -0,0 +1,13 @@
+/**
+ * 弹出窗口主控程序
+ * Created by AnCla on 2016/9/8 0008.
+ */
+
+        function show_popup_window(){
+            document.getElementById("shader").style.visibility="visible";
+            document.getElementById("popup-window").style.visibility="visible";
+        }
+        function hide_popup_window(){
+            document.getElementById("popup-window").style.visibility="hidden";
+            document.getElementById("shader").style.visibility="hidden";
+        }
